532f7fe780b8adcb5114429d581c9c60fd5be494
Samuel Solomon <sammygideon01@gmail.com>
2022-07-02 07:31:37 +0000

    Add an addEventListener to the number, operator and equal buttons

--- javascript.js
+++ javascript.js
@@ -1,45 +1,80 @@
 // javascript
 
 function add(num1, num2) {
-    let result = num1 + num2;
+    let result = parseInt(num1) + parseInt(num2);
     return result;
 }
 
 function subtract(num1, num2) {
-    let result = num1 - num2;
+    let result = parseInt(num1) - parseInt(num2);
     return result;
 }
 
 function multiply(num1, num2) {
-    let result = num1 * num2;
+    let result = parseInt(num1) * parseInt(num2);
     return result;
 }
 
 function divide(num1, num2) {
-    let result = num1 / num2;
+    let result = parseInt(num1) / parseInt(num2);
     return result;
 }
 
-const operator = document.querySelectorAll(".operator")
+const displayArea = document.querySelector("#display");
+const numbers = document.querySelectorAll("#number");
+const operators = document.querySelectorAll(".operator");
+const equal = document.querySelector(".equal");
+const deletes = document.querySelector(".delete");
+const clear = document.querySelector(".clear");
+
 function operate(num1, operator, num2) {
-    if (operator = "add") {
-        console.log(add());
-    } else if (operator = "subtract") {
-        return subtract();
-    } else if (operator = "multiply") {
-        return multiply();
-    } else if (operator = "divide") {
-        return divide();
+    let result = 0;
+    switch (operator) {
+        case '+':
+            result = add(num1, num2);
+            break;
+        case '-':
+            result = subtract(num1, num2);
+            break;    
+        case '*':
+            result = multiply(num1, num2);
+            break;
+        case '/':
+            result = divide(num1, num2);
+            break;    
+        default:
+            break;
     }
+    return result;
 }
 
-let displayValue = 0;
-let displayArea = document.getElementById("display");
+let num1 = "";
+let num2 = "";
+let operator = "";
+operators.forEach((op) => {
+    op.addEventListener("click", (e) => {
+        operator = e.target.innerText;
+        displayArea.innerText = num1;
+        return operator;
+    });
+});
 
-const buttons = document.querySelectorAll("#number");
-buttons.forEach((number) => {
+numbers.forEach((number) => {
     number.addEventListener("click", (e) => {
-        displayValue = e.target.innerText;
-        displayArea.innerText = displayValue;
-    })
+        if (operator === "") {
+            num1 += e.target.innerText;
+            displayArea.innerText = num1;
+        } else {
+            num2 += e.target.innerText;
+            displayArea.innerText = num2;
+        }
+    });
+});
+
+equal.addEventListener("click", (e) => {
+    if (e.target.innerText = "=") {
+        displayArea.innerText = num2;
+        let result = operate(num1, operator, num2);
+        displayArea.innerText = result;
+    }
 })
